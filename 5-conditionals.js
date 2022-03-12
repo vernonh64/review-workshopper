c0498de51777cf607c7b77c51d7c60058069c938
vernonh64 <94575474+vernonh64@users.noreply.github.com>
2022-03-12 14:04:25 -0500

    On one condition

--- 5-conditionals.js
+++ 5-conditionals.js
@@ -1,8 +1,7 @@
 let overOrUnder = 69;
 
 //if(overOrUnder > 50){console.log("Over 50")}else{console.log("under 50")}
-if(overOrUnder<0 || overOrUnder>100){ 
-    console.log("outside Third")
+if(overOrUnder<0 || overOrUnder>100){ console.log("outside Third")
 }else if(overOrUnder<33){console.log("Bottom Third")
 }else if(overOrUnder>=33 && overOrUnder<=66){console.log("Middle Third")
 } else{console.log("Top Third")}
