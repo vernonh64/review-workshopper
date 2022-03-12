ee9ccb2b439ec08f9cabd6732bac16229406008c
vernonh64 <94575474+vernonh64@users.noreply.github.com>
2022-03-12 14:00:30 -0500

    #Displayed

--- 1-displaying-output.js
+++ 1-displaying-output.js
@@ -2,11 +2,9 @@
 
 console.log(`Hey now, youre an all-star, get your game on, go play
 Hey now, youre a rock star, get the show on, get paid`)
-
 console.log(`my current age is 31`)
 
 let sentence = `I would like to thank my arms for always being by my side, my legs for always supporting me, and my fingers, because I can always count on them.`
-
 let wordslist = sentence.split(" ")
 
 
