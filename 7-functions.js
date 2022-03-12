8374b7ed651fad55ef34085e423a34e863ca3527
vernonh64 <94575474+vernonh64@users.noreply.github.com>
2022-03-12 14:06:01 -0500

    On one condition

--- 7-functions.js
+++ 7-functions.js
@@ -1,14 +1,11 @@
 function howLong(str, num){
-
     let sLength = str.length
-    
     if(sLength>num){return true} else{return false}
-
 }
 
 function caller(str, num){
-console.log(howLong(str, num))
-}
+  console.log(howLong(str, num))
+   }
 
 caller("five", 5)
 caller("fiverr", 5)
