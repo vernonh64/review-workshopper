c6a492eadbe300a65f237de3ad7c1239e3e42598
vernonh64 <94575474+vernonh64@users.noreply.github.com>
2022-03-12 14:05:15 -0500

    On one condition

--- 6-loops.js
+++ 6-loops.js
@@ -1,20 +1,17 @@
 function display(){
 for(let i = 0; i<=50; i++){
-    console.log(i)
-}
+    console.log(i)}
 }
 
 let stop = 23;
 function breakIt(){
 for(let i = 0; i<=50; i++){
-    console.log(i)
-}
+    console.log(i)}
 }
 
 function sayIt(){
 for(let i = 0; i<=50; i++){
-   if(i%2==0){console.log(i)}else{continue}
-}
+   if(i%2==0){console.log(i)}else{continue} }
 }
 
 
